bd31be0b8e343d52f1268c97832c3cbca24f56a3
Erfan Saeedian <erfuniism@gmail.com>
2026-05-19 15:36:25 +0330

    fix

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import totalCards from "../components/common/totalCards";
+import TotalCards from "../components/common/totalCards";
 import Container from "../components/layout/container";
 import Content from "../components/layout/content";
 import TableList from "../components/common/tabelList";
