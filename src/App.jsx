fb3ee9a5eea2de45785aec067a6ad5f4d2545134
Erfan Saeedian <erfuniism@gmail.com>
2026-05-20 20:43:00 +0330

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Appointments from "./pages/appointments";
 import ClinicInfo from "./pages/clinicinfo";
 import Doctors from "./pages/doctors";
 import Patients from "./pages/patients";
-import phatmacies from "./pages/phatmacies";
+// import phatmacies from "./pages/phatmacies";
 import Schedule from "./pages/schedule";
 import Staff from "./pages/staff";
 import Dashboard from "./pages/dashboard";
@@ -18,7 +18,7 @@ const App = () => {
         <Route path="/doctors" element={<Doctors />} />
         <Route path="/staff" element={<Staff />} />
         <Route path="/ptients" element={<Patients />} />
-        <Route path="/pharmacies" element={<Phatmacies />} />
+        {/* <Route path="/pharmacies" element={<Phatmacies />} /> */}
         <Route path="/schedule" element={<Schedule />} />
         <Route path="/clinic-info" element={<ClinicInfo />} />
       </Routes>
