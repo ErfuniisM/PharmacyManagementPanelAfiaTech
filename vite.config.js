82b0fd57a1cb0c72de1cad0dabd370edb8d9caf4
Erfan Saeedian <erfuniism@gmail.com>
2026-05-27 12:23:30 +0330

    Test App-4

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "./",
   plugins: [react(), tailwindcss()],
-  // server: {
-  //   port: 5500,
-  //   strictPort: true,
-  // },
 });
