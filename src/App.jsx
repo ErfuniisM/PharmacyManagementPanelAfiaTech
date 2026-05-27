f06576dbfef08ee88ccf4389c35bff5ea45aa4e7
Erfan Saeedian <erfuniism@gmail.com>
2026-05-27 12:15:25 +0330

    Test App-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,38 @@
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-//   Navigate,
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
-// import Appointments from "./pages/appointments";
-// import ClinicInfo from "./pages/clinicinfo";
-// import Doctors from "./pages/doctors";
-// import Patients from "./pages/patients";
-// import Schedule from "./pages/schedule";
-// import Staff from "./pages/staff";
-// import Dashboard from "./pages/dashboard";
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Navigate to="/dashboard" />} />
-
-//         <Route path="/dashboard" element={<Dashboard />} />
-//         <Route path="/appointments" element={<Appointments />} />
-//         <Route path="/doctors" element={<Doctors />} />
-//         <Route path="/staff" element={<Staff />} />
-
-//         <Route path="/patients" element={<Patients />} />
-
-//         <Route path="/schedule" element={<Schedule />} />
-//         <Route path="/clinic-info" element={<ClinicInfo />} />
-
-//         <Route path="*" element={<Navigate to="/dashboard" />} />
-//       </Routes>
-//     </Router>
-//   );
-// };
-
-// export default App;
+import Appointments from "./pages/appointments";
+import ClinicInfo from "./pages/clinicinfo";
+import Doctors from "./pages/doctors";
+import Patients from "./pages/patients";
+import Schedule from "./pages/schedule";
+import Staff from "./pages/staff";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/Dashboard" />} />
+
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/appointments" element={<Appointments />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/staff" element={<Staff />} />
+
+        <Route path="/patients" element={<Patients />} />
+
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/clinic-info" element={<ClinicInfo />} />
+
+        <Route path="*" element={<Navigate to="/Dashboard" />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
